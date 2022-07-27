3ae899f63ffa4a88364dcb05acfa7c3bf3d55753
Ivano Garcia <ivano.garcia@globant.com>
2022-07-26 23:39:04 -0400

    feature: Update the packages and improving the serverless.yml

--- src/handler/hello.js
+++ src/handler/hello.js
@@ -1,9 +1,12 @@
 'use strict';
+const Logger = require('../utils/logger');
 
+const logger = new Logger()
 const { success, error } = require('../provider/http-response');
 
 module.exports.handler = async (event, context) => {
     try {
+        logger.info("Hello Handle");
         const body = "Serverless Success!"
         return success(body, 200)
     } catch (error) {
